9e80bfe0418e07cf7870bc45e6c554534bd43340
monkeylord <sreenuyedvalli@gmail.com>
2017-11-20 13:14:28 -0500

    added update and delete functions..learned about blank identifiers

--- model.go
+++ model.go
@@ -23,3 +23,35 @@ func (n *namest) createName(db *sql.DB) error {
 
     return nil
 }
+
+func (n *namest) updateCount(db *sql.DB) error {
+    _, err :=
+        db.Exec("UPDATE names SET count = count + 1 WHERE name=$1",
+            n.Name)
+
+    return err
+}
+
+func getNames(db *sql.DB, start, count int) ([]namest, error) {
+    rows, err := db.Query(
+        "SELECT name, count FROM names LIMIT $1 OFFSET $2",
+        count, start)
+
+    if err != nil {
+        return nil, err
+    }
+
+    defer rows.Close()
+
+    names := []namest{}
+
+    for rows.Next() {
+        var n namest
+        if err := rows.Scan(&n.Name, &n.Count); err != nil {
+            return nil, err
+        }
+        names = append(names, n)
+    }
+
+    return names, nil
+}
