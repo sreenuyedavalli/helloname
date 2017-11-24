e6111919e884dfc8c9ecc40fd57ee18388d3a143
sreenuyedavalli <sreenuyedavalli@gmail.com>
2017-11-24 08:26:25 -0500

    updated comments on db functions

--- model.go
+++ model.go
@@ -6,11 +6,13 @@ import (
 	"database/sql"
 )
 
+//Primary struct for marshelling and unmarshalling data to a from postgres
 type namest struct {
 	Name  string `json:"name"`
 	Count int    `json:"count"`
 }
 
+//createName - takes name and inserts new row if not found from getName increases count by 1
 func (n *namest) createName(db *sql.DB) error {
 	err := db.QueryRow(
 		"INSERT INTO names(name) VALUES($1) RETURNING name",
@@ -24,6 +26,7 @@ func (n *namest) createName(db *sql.DB) error {
 	return nil
 }
 
+//updateCount - takes getName and if a match is found increases count by 1 
 func (n *namest) updateCount(db *sql.DB) error {
 	_, err :=
 		db.Exec("UPDATE names SET count = count + 1 WHERE name=$1",
@@ -32,6 +35,7 @@ func (n *namest) updateCount(db *sql.DB) error {
 	return err
 }
 
+//getNames - search and return all names
 func getNames(db *sql.DB, start, count int) ([]namest, error) {
 	rows, err := db.Query(
 		"SELECT name, count FROM names LIMIT $1 OFFSET $2",
@@ -56,12 +60,13 @@ func getNames(db *sql.DB, start, count int) ([]namest, error) {
 	return names, nil
 }
 
+//getName - Scans name table to find match from json input
 func (n *namest) getName(db *sql.DB) error {
     return db.QueryRow("SELECT name FROM names WHERE name=$1",
         n.Name).Scan(&n.Name)
 }
 
-
+//deleteCounts - from delete post call delete all counts in count column
 func (n *namest) deleteCounts(db *sql.DB) error {
 	_, err := db.Exec("UPDATE names SET count=0 WHERE count>0")
 
